b83862af37743ffac1caa00fe29e0612b989fb44
mirjSolution <ricojohndatoon23@gmail.com>
2021-01-27 15:00:31 -0500

    Add Pagination coomponent

---
+++ src/Pagination.js
@@ -0,0 +1,6 @@
+import { useState } from 'react';
+
+function Pagination({ data, RenderComponent, title, pageLimit, dataLimit }) {
+  const [pages] = useState(Math.round(data.length / dataLimit));
+  const [currentPage, setCurrentPage] = useState(1);
+}
